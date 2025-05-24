01577b600de6a227d788f7da585ba1e226533771
Matthew Black <mblackmblackmblack@gmail.com>
2025-05-23 19:18:14 -0700

    build: extract prebuild scripts to shell files
    - Move prebuild_nodejs and prebuild_electron commands to separate .sh files
    - Format arguments with newlines for improved maintainability
    - Update package.json to reference new shell scripts

---
+++ prebuild_electron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+prebuild \
+  --backend cmake-js \
+  -t 7.0.0 \
+  -t 7.3.0 \
+  -t 8.0.0 \
+  -t 9.0.0 \
+  -r electron \
+  --verbose \
+  --include-regex ".*(node|a|so|dll|dylib)$" \
+  -- \
+  -O build \
+  --CDENABLE_SHARED=off \
+  --CDENABLE_CAPI=off \
+  --CDENABLE_TESTS=off \
+  --CDENABLE_JSONAPI=off \
+  --CDENABLE_JS_WRAPPER=off \
+  --CDTARGET_RPATH="@executable_path;/usr/local/lib;/usr/local/lib64;\$\$ORIGIN/node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/../node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/../../node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/../../../node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/../../../../node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/../../../../../node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/../../../../../../node_modules/cfd-dlc-js/wrap_js/build/Release;\$ORIGIN/build/Release;\$ORIGIN/wrap_js/build/Release;\$ORIGIN/wrap_js/dl_lib/lib" \
+  -C \
+  --CDnapi_build_version=3 
---
+++ prebuild_nodejs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+prebuild \
+  --backend cmake-js \
+  -t 20.18.0 \
+  -t 18.18.2 \
+  -t 16.20.2 \
+  -t 15.1.0 \
+  -t 14.18.1 \
+  -t 12.18.1 \
+  -r node \
+  --verbose \
+  --include-regex ".*(node|a|so|dll|dylib)$" \
+  -- \
+  -O build \
+  --CDENABLE_SHARED=off \
+  --CDENABLE_CAPI=off \
+  --CDENABLE_TESTS=off \
+  --CDENABLE_JSONAPI=off \
+  --CDENABLE_JS_WRAPPER=off \
+  --CDTARGET_RPATH="@executable_path;/usr/local/lib;/usr/local/lib64;./node_modules/cfd-dlc-js/wrap_js/build/Release;../node_modules/cfd-dlc-js/wrap_js/build/Release;../../node_modules/cfd-dlc-js/wrap_js/build/Release;../../../node_modules/cfd-dlc-js/wrap_js/build/Release;../../../../node_modules/cfd-dlc-js/wrap_js/build/Release;../../../../../node_modules/cfd-dlc-js/wrap_js/build/Release;../../../../../../node_modules/cfd-dlc-js/wrap_js/build/Release;./build/Release;./wrap_js/build/Release;./wrap_js/dl_lib/lib" \
+  -C \
+  --CDnapi_build_version=3 
